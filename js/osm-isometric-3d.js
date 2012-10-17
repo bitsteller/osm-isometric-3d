5de090b43f517d4a6b99fe864a6cb319f6d2fa3d
Nils Breyer <nbnet@arcor.de>
2012-10-17 20:48:43 +0200

    prevent caching for status.json

--- js/osm-isometric-3d.js
+++ js/osm-isometric-3d.js
@@ -55,7 +55,7 @@ function loadCities() {
 
 function loadStatus(repeat) {
 	var http_request = new XMLHttpRequest();
-	http_request.open("GET", "status.json", true);
+	http_request.open("GET", "status.json?bustCache=" + Math.random(), true);
 	http_request.onreadystatechange = function () {
 		var done = 4, ok = 200;
 		if (http_request.readyState == done && http_request.status == ok) {
